d03fa88f2d56b7a0d474906d26ee4b4258a0fc0d
mcagovind980 <kg878672@gmail.com>
2026-08-12 21:36:58 +0530

    nodejs

--- index.js
+++ index.js
@@ -19,14 +19,45 @@
 //     console.log("Server running on port 3000");
 // });
 
-const fs=require("fs");
-fs.readFile("index.html","utf-8",(err,data)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
-})
-console.log("read file...()this runs first");
+// const fs=require("fs");
+// fs.readFile("index.html","utf-8",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(data);
+//     }
+// })
+// console.log("read file...()this runs first");
 
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const fs=require('fs')
+const path=require('path')
+const pathDir=path.join(__dirname,"crus")
+
+const filePath=`${pathDir}/demo.txt`
+console.log(pathDir);
+
+// fs.writeFileSync(filePath,"hello node students")
+
+// fs.readFile(filePath,'utf-8',(err,item)=>
+// {
+//     console.log(item);
+    
+// })
+
+// fs.appendFile(filePath,'file name is crud',(err)=>
+// {
+//     if(!err) console.log("  Flie is updated");
+    
+// })
+
+// fs.rename(filePath,`${pathDir}/demo1.txt`,(err)=>
+// {
+//     if(!err) console.log("file is rename");
+// })
+
+// fs.unlinkSync(`${pathDir}/demo1.txt`)
 
