cd0db1f3b3784ce49554991286b4affea505b1c9
mcagovind980 <kg878672@gmail.com>
2026-08-12 15:06:53 +0530

    readfile nodejs

--- index.js
+++ index.js
@@ -9,13 +9,24 @@
     
 // })
 
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.end("Hello from Node.js!");
-});
+// const server = http.createServer((req, res) => {
+//     res.end("Hello from Node.js!");
+// });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+// server.listen(3000, () => {
+//     console.log("Server running on port 3000");
+// });
+
+const fs=require("fs");
+fs.readFile("index.html","utf-8",(err,data)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    }
+})
+console.log("read file...()this runs first");
+
 
